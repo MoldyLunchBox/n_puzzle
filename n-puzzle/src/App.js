b7db69b2db28bad0911b2424f269727f7f6300c1
Abderrahmane Mya <amya@e1r7p10.1337.ma>
2023-02-18 01:05:01 +0100

    grid

--- n-puzzle/src/App.js
+++ n-puzzle/src/App.js
@@ -3,33 +3,56 @@ import './App.css';
 import {Row, Col, InputNumber, Form, Button} from  "antd";
 import {useState} from "react";
 import { solver } from './solver';
+const { log } = console;
+
 
 function App() {
   const [values, setValues] = useState([]);
    const onFinish = (values: any) => {
-    const newArray = Object.values(values);
-    setValues(newArray)
-
+    values.preventDefault()
+    log(values.target[2].value)
   };
-  console.log(values)
-  solver()
   return (
     <div className="App">
-      <Form onFinish={onFinish}>
-        <Row>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="1"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="2"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="3"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="4"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="5"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="6"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="7"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="8"><InputNumber /></Form.Item></div></Col>
-          <Col span={8} ><div style={{ width:"100%", height:"200px", border:"1px solid black" }}><Form.Item name="9"><InputNumber /></Form.Item></div></Col>
-        </Row>
-        <Button htmlType="submit">submit</Button>
-      </Form>
-
+      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <form onSubmit={onFinish}>
+        <div className="w-full max-w-screen-lg mx-auto flex">
+          <div className="w-2/3">
+            <div className="grid grid-cols-3 grid-rows-3 gap-4">
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input id="1" type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+              <div className="bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+                <input type="number" min="1" max="9" className="w-full h-full" />
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 bg-gray-100 flex items-center justify-center">
+            <button type="submit" className="px-4 py-2 rounded bg-blue-500 text-white">Submit</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
