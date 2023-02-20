b342c9b4b1dff37e5c5311c465a317a02790bf31
Abderrahmane Mya <amya@e1r9p10.1337.ma>
2023-02-21 00:41:12 +0100

    grid

--- n-puzzle/src/App.js
+++ n-puzzle/src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import Grid from './components/Grid';
+import './App.scss';
+
 import DropDownMenu from './components/DropDownMenu';
 
 import logo from './logo.svg';
 import './App.css';
 import { Row, Col, InputNumber, Form, Button } from "antd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { solver } from './solver';
 const { log } = console;
 
 function App() {
   const [values, setValues] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
   const [mapSize, setMapSize] = useState(3)
-  const mapSizeChange = (mapSize1) =>{
-    const val = mapSize1.target.value
-    setMapSize(val)
-  
-
-  }
-  const onFinish = (values) =>{
+  useEffect(() => {
+    const sizeMap = document.getElementById("mapSzie").value;
+      const gridContainer = document.querySelector('.grid-Container');
+  gridContainer.style.gridTemplateColumns = `repeat(${sizeMap}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${sizeMap}, 100px)`;
+  });
+const mapSizeChange = (event) => {
+  const val = event.target.value;
+  setMapSize(val);
+  const gridContainer = document.querySelector('.grid-Container');
+  gridContainer.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${val}, 100px)`;
+};
+  const onFinish = (values) => {
     values.preventDefault()
     log(values)
     const inputs = Array.from(document.querySelectorAll('input[type="number"]'));
@@ -27,19 +36,26 @@ function App() {
     console.log(numbers);
   };
   return (
+    
     <div className="App">
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
-
+      <div className="flex mb-4">
+        <div className="w-1/3 bg-gray-400 h-12"></div>
+      <h1 className="w-1/3 bg-gray-500 h-12 text-3xl text-white font-bold"> N puzzle </h1>
+        <div className="w-1/3 bg-gray-400 h-12"></div>
+      </div>
       <form onSubmit={onFinish}>
         <div className="w-full max-w-screen-lg mx-auto flex">
           <div className="w-2/3">
-            <Grid numbers={values}  mapSize={mapSize} />
+          <div className="grid-Container">
+       
+            <Grid numbers={values} mapSize={mapSize} />
+          </div>
           </div>
           <div className="w-1/3 bg-gray-100 flex items-center">
             <div className="h-full flex flex-col justify-center border-4 border-indigo-200 border-r-indigo-500">
-              <div className="divide-y divide-slate-200 ...">
-             <div> <DropDownMenu mapSizeChange={mapSizeChange} /></div>
-                <div>              <button type="submit" className="px-4  my-5 mx-5 px-5  py-2 rounded bg-blue-500 text-white">Submit</button>
+              <div className="divide-y divide-slate-200 w-24 mx-5">
+                <div> <DropDownMenu className="my-10" mapSizeChange={mapSizeChange} /></div>
+                <div>  <button type="submit" className="my-5 py-2  w-full rounded bg-blue-500 text-white">Submit</button>
                 </div>
               </div>
 
--- n-puzzle/src/components/Grid.js
+++ n-puzzle/src/components/Grid.js
@@ -8,13 +8,13 @@ function Grid(props) {
   const gridItems = [];
   for (let i = 0; i < mapSize * mapSize; i++) {
     gridItems.push(
-      <div key={i} className="bg-gray-200 p-4 w-12 h-12">
-        <input type="number" min="1" max="9" className="w-8 h-8" />
+      <div key={i} className="h-full bg-gray-200 p-4 aspect-w-1 aspect-h-1">
+        <input type="number" min="1" max="9" className="w-full h-full text-3xl" />
       </div>
     );
   }
 
-  return <div className={`grid grid-cols-${mapSize} grid-rows-${mapSize} gap-4`}>{gridItems}</div>;
-}
+return gridItems;
 
+}
 export default Grid;
