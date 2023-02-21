c588f9258f16b6de436992ba2f554c95aaa3dadd
Abderrahmane Mya <amya@e1r3p1.1337.ma>
2023-02-22 00:01:46 +0100

    solution steps

--- n-puzzle/src/components/PuzzleSteps.js
+++ n-puzzle/src/components/PuzzleSteps.js
@@ -1,36 +1,30 @@
 import { useState, useEffect } from "react";
 import react from 'react'
 import Grid from "./Grid"
-const {log} = console
+const { log } = console
 
-const PuzzleSteps = (props) =>{
+const PuzzleSteps = (props) => {
 
-    const {validNumbers, numbers, mapSize} = props
-    log("new")
-    log(mapSize)
-    const col = `grid-template-columns: repeat(${mapSize}, 1fr)`;
-    log(col)
-    return(
-        <div className="w-full max-w-screen-lg mx-auto flex">
-          <div className="w-2/3">
-            <div className="grid-Container2" style={`grid-template-columns: repeat(${mapSize}, 1fr)`}>
+  const { validNumbers, numbers, mapSize } = props
+  log("new")
+  log(mapSize)
+  const col = `repeat(${mapSize}, 1fr)`;
 
-              <Grid validNumbers={validNumbers} numbers={numbers} mapSize={mapSize} />
-            </div>
-          </div>
-          <div className="w-1/3 bg-gray-100 flex items-center">
-            <div className="h-full flex flex-col justify-center border-4 border-indigo-200 border-r-indigo-500">
-              <div className=" w-24 mx-5">
-                  <button type="submit" className="my-5 py-2  w-full rounded bg-blue-400 text-white">Submit</button>
-                  <button  className="my-5 py-2  w-full rounded bg-red-400 text-white">Clear</button>
-               
-              </div>
-
-
-            </div>
-          </div>
+  return (
+    <div className="w-full max-w-screen-lg mx-auto flex">
+      <div className="w-2/3">
+        <div className="grid-Container2" style={{ display: 'grid', gridTemplateColumns: col }}>
+          <Grid validNumbers={validNumbers} numbers={numbers} mapSize={mapSize} />
         </div>
-    )
+      </div>
+      <div className="w-1/3 bg-gray-100 flex items-center border-4 border-indigo-200 border-r-indigo-500 justify-center">
+        <div className="flex flex-row w-2/3">
+          <button type="submit" className="mx-5 py-2 w-full rounded bg-blue-400 text-white">Left</button>
+          <button className="mx-5 py-2 w-full rounded bg-red-400 text-white">Right</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PuzzleSteps
